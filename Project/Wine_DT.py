f33d7e9ec879784242b057567dd2f58a90acb030
BehnamTajzadeh <behnam.taj99@gmail.com>
2026-01-05 12:31:03 +0330

    Project_Add_Scaler
    Final Project - added Standard Scaler to normalize data

--- Project/Wine_DT.py
+++ Project/Wine_DT.py
@@ -1,13 +1,17 @@
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, mean_squared_error, accuracy_score
 
 wineData = load_wine()
 X = wineData.data
 y = wineData.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 dt = DecisionTreeClassifier()
 dt.fit(X_train, y_train)
